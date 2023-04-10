8c325093b577871df81fe5338f67df6b182b5742
Donovan Lilov <donovanlilov@gmail.com>
2023-04-09 18:07:14 -0700

    changes

---
+++ foodrumor-server/middleware/auth.js
@@ -0,0 +1,41 @@
+//for some reason has .load in the video
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+exports.loginRequired = function(req, res, next) {
+    try{
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+        if (decoded) {
+            return next();
+        }else{
+            return next({
+                status: 401,
+                message: "Please log in first"
+
+            });
+        }
+    });
+//could be catch (err)
+} catch (e) {
+    return next ({status: 401, message: "Please log in first"});
+    }   
+};
+exports.ensureCorrectUser = function(req, res, next) {
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+            if (decoded && decoded.id === req.params.id) {
+                return next();
+            } else {
+                return next ({
+                    status: 401,
+                    message: "Unauthorized"
+                });
+            }
+        });
+        //also couud be catch (err)
+    } catch(e){
+        return next({status: 401, message: "Unauthorized"});
+    }
+};
